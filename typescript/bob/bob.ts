330a636faa7c159122db9dba447bbabdf8e6478f
skarode@poppulo.com <skarode@poppulo.com>
2022-06-25 23:31:02 +0100

    add bob solution

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,3 +1,22 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+function isYelling(message: string) {
+  return message.toUpperCase() === message;
+}
+function isAskingQuestion(message:string) {
+  return message[message.length - 1] === "?";
+}
+
+function hasLetters(message:string) {
+  return /[a-zA-Z]/.test(message);
+}
+export function hey(message: string): string {
+  const trimmedMsg = message.replace(/\s/g, "");
+  if(trimmedMsg === "")
+    return "Fine. Be that way!"
+  else if (hasLetters(trimmedMsg) && isYelling(trimmedMsg) && !isAskingQuestion(trimmedMsg))
+    return 'Whoa, chill out!';
+  else if (hasLetters(trimmedMsg) && isYelling(trimmedMsg) && isAskingQuestion(trimmedMsg))
+    return "Calm down, I know what I'm doing!";
+  else if (isAskingQuestion(trimmedMsg))
+    return "Sure."
+  return 'Whatever.';
 }
